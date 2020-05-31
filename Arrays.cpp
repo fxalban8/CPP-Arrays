a0999964d37ef1fc2af1e78cf0eabc44c7f533b5
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-31 09:15:11 -0500

    Declaring an array, assigning values and printing them all

---
+++ Arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	//declaring an array whose elements are integers
+	int array[3];
+	
+	//assigning values to each element of the array
+	array[0]=7;
+	array[1]=-5;
+	array[2]=9;
+
+	//printing each element of the array
+	cout<<"array[0]= "<<array[0]<<endl;
+	cout<<"array[1]= "<<array[1]<<endl;
+	cout<<"array[2]= "<<array[2]<<endl;
+	
+	return 0;
+}
